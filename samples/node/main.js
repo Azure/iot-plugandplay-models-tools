5f92e59f474f7ce236b50629cb58e371f60ffa96
ridomin <ridomin@live.com>
2020-09-30 12:58:36 -0700

    align dtmi2path with dotnet

--- samples/node/main.js
+++ samples/node/main.js
@@ -9,11 +9,7 @@ const https = require('https')
  */
 const dtmi2path = dtmi => {
   if (RegExp('^dtmi:[A-Za-z](?:[A-Za-z0-9_]*[A-Za-z0-9])?(?::[A-Za-z](?:[A-Za-z0-9_]*[A-Za-z0-9])?)*;[1-9][0-9]{0,8}$').test(dtmi)) {
-    const idAndVersion = dtmi.toLowerCase().split(';')
-    const ids = idAndVersion[0].split(':')
-    const fileName = `${ids.pop()}-${idAndVersion[1]}.json`
-    const modelFolder = ids.join('/')
-    return `/${modelFolder}/${fileName}`
+    return `/${dtmi.toLowerCase().replace(/:/g, '/').replace(';', '-')}.json`
   } else return 'NOT-VALID-DTMI'
 }
 
