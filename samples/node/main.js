43ee30bd27a44b84aa9bd5a2b795bcb660f80b11
Ricardo Minguez (Rido) <rido-min@users.noreply.github.com>
2020-09-30 17:16:53 -0700

    Rido/node sample (#52)
    * Add node sample
    
    * fix typos
    
    * add ref for regex
    
    * add reged ref to the README
    
    * align dtmi2path with dotnet
    
    * return abs path
    
    Co-authored-by: ridomin <ridomin@live.com>

--- samples/node/main.js
+++ samples/node/main.js
@@ -2,18 +2,14 @@
 const https = require('https')
 
 /**
- * @description Converts DTMI to dtmi/com/example/device-1.json path.
+ * @description Converts DTMI to /dtmi/com/example/device-1.json path.
  *   Validates DTMI with RegEx from https://github.com/Azure/digital-twin-model-identifier#validation-regular-expressions
  * @param {string} dtmi
  * @returns {string}
  */
 const dtmi2path = dtmi => {
   if (RegExp('^dtmi:[A-Za-z](?:[A-Za-z0-9_]*[A-Za-z0-9])?(?::[A-Za-z](?:[A-Za-z0-9_]*[A-Za-z0-9])?)*;[1-9][0-9]{0,8}$').test(dtmi)) {
-    const idAndVersion = dtmi.toLowerCase().split(';')
-    const ids = idAndVersion[0].split(':')
-    const fileName = `${ids.pop()}-${idAndVersion[1]}.json`
-    const modelFolder = ids.join('/')
-    return `/${modelFolder}/${fileName}`
+    return `/${dtmi.toLowerCase().replace(/:/g, '/').replace(';', '-')}.json`
   } else return 'NOT-VALID-DTMI'
 }
 
