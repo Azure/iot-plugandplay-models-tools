0ec5bbc185792b11e157f923b5fe643018200345
Paul Montgomery <pamontg@microsoft.com>
2020-10-19 11:14:15 -0700

    Move the improt flow to ModelImportrer.cs

--- resolvers/dotnet/Azure.DigitalTwins.Resolver.CLI/Program.cs
+++ resolvers/dotnet/Azure.DigitalTwins.Resolver.CLI/Program.cs
@@ -253,7 +253,7 @@ namespace Azure.DigitalTwins.Resolver.CLI
                 var parser = GetParser(repository.FullName, logger);
                 try
                 {
-                    var newModels = await importModels(modelFile, repository, force, logger);
+                    var newModels = await ModelImporter.importModels(modelFile, repository, force, logger);
                     foreach(var model in newModels)
                     {
                         var validationResult = await validateFile(model, repository.FullName, true, logger, parser);
@@ -272,87 +272,7 @@ namespace Azure.DigitalTwins.Resolver.CLI
 
 
             });
-
             return addModel;
-        }
-
-        private static async Task<IEnumerable<FileInfo>> importModels(FileInfo modelFile, DirectoryInfo repository, bool force, ILogger logger)
-        {
-            var fileText = await File.ReadAllTextAsync(modelFile.FullName);
-            var model = JsonDocument.Parse(fileText);
-
-            return importModels(model, modelFile.FullName, repository, force, logger);
-        }
-
-        private static IEnumerable<FileInfo> importModels(JsonDocument document, string fileName, DirectoryInfo repository, bool force, ILogger logger)
-        {
-            var root = document.RootElement;
-            if (root.ValueKind == JsonValueKind.Array)
-            {
-                logger.LogInformation($"Array found in {fileName}");
-                var enumerable = root.EnumerateArray();
-                foreach (var modelItem in enumerable)
-                {
-
-                    yield return importModel(modelItem, fileName, repository, logger, force);
-                }
-            }
-            else
-            {
-                logger.LogInformation($"Single item found in {fileName}");
-                yield return importModel(root, fileName, repository, logger, force);
-
-            }
-        }
-
-        private static FileInfo importModel(JsonElement modelItem, string fileName, DirectoryInfo repository, ILogger logger, bool force)
-        {
-            //Do DTMI verification
-            var rootId = Validations.GetRootId(modelItem, fileName);
-            if (!Validations.IsDtmi(rootId.GetString()))
-            {
-                throw new InvalidDTMIException(rootId);
-            }
-            if (!Validations.ValidateDTMIs(modelItem, fileName, logger))
-            {
-                throw new InvalidDTMIException(fileName);
-            }
-            if(!Validations.ScanForReservedWords(modelItem.ToString(), logger))
-            {
-                throw new ValidationException($"File '{fileName}' contains reserved words.");
-            }
-            if(!Validations.ValidateContext(modelItem, fileName, logger))
-            {
-                throw new ValidationException($"File '{fileName}' is missing @context element");
-            }
-            
-            // write file to repository location
-            var newFile = $"{rootId.GetString().Replace(';', '-').Replace(':', Path.DirectorySeparatorChar).ToLower(CultureInfo.InvariantCulture)}.json";
-            var newPath = Path.Join(repository.FullName, newFile);
-            if (!File.Exists(newPath))
-            {
-                CheckCreateDirectory(newPath);
-                logger.LogInformation($"Writing new file to '{newPath}'.");
-                File.WriteAllText(newPath, modelItem.ToString(), Encoding.UTF8);
-            } else if (force) {
-                logger.LogWarning($"File '{newPath} already exists. Overwriting...");
-                File.WriteAllText(newPath, modelItem.ToString(), Encoding.UTF8);
-            } else {
-                throw new IOException($"File '{newPath} already exists. Remove or use '--force' to overwrite.");
-            }
-
-            //return file info
-            return new FileInfo(newPath);
-        }
-
-        private static void CheckCreateDirectory(string filePath)
-        {
-            var lastDirectoryIndex = filePath.LastIndexOf(Path.DirectorySeparatorChar);
-            var directory = filePath.Substring(0, lastDirectoryIndex);
-            if(!Directory.Exists(directory))
-            {
-                Directory.CreateDirectory(directory);
-            }
         }
     }
 }
