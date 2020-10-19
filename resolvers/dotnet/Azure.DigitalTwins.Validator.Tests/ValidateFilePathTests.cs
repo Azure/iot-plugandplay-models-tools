c509b52e99eb75b447aa743beb96b22a4bb7c085
Paul Montgomery <pamontg@microsoft.com>
2020-10-19 13:06:30 -0700

    Remove FileInfo tests to avoid error in GH Action

--- resolvers/dotnet/Azure.DigitalTwins.Validator.Tests/ValidateFilePathTests.cs
+++ resolvers/dotnet/Azure.DigitalTwins.Validator.Tests/ValidateFilePathTests.cs
@@ -14,26 +14,6 @@ namespace Azure.DigitalTwins.Validator.Tests
         }
 
         [Test]
-        public void ValidatesFileInfo()
-        {
-            var fileInfo = new System.IO.FileInfo("./TestModelRepo/dtmi/azure/devicemanagment/deviceinformation-1.json");
-            Assert.True(Validations.ValidateFilePath(fileInfo.FullName));
-        }
-
-        [Test]
-        public void ValidatesFileInfoWindowsSlashes()
-        {
-            var fileInfo = new System.IO.FileInfo(".\\TestModelRepo\\dtmi\\azure\\devicemanagement\\deviceinformation-1.json");
-            Assert.True(Validations.ValidateFilePath(fileInfo.FullName));
-        }
-        [Test]
-        public void FailsOnImproperFolderStructure()
-        {
-            var fileInfo = new System.IO.FileInfo(".\\badfile\\deviceinformation-1.json");
-            Assert.False(Validations.ValidateFilePath(fileInfo.FullName));
-        }
-
-        [Test]
         public void FailsOnUppercasing()
         {
             Assert.False(Validations.ValidateFilePath("dtmi\\com\\example\\Thermostat-1.json"));
