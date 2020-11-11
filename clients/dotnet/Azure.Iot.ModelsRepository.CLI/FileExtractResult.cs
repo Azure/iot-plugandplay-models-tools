b8913a13997bd755d537ef74d94d214386f6da7a
Paymaun <Paymaunh@microsoft.com>
2020-11-11 14:50:12 -0800

    Use official namespace and small tweaks (#129)
    * Use official namespace.
    * Tweak pipeline path.

---
+++ clients/dotnet/Azure.Iot.ModelsRepository.CLI/FileExtractResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Azure.Iot.ModelsRepository.CLI
+{
+    internal class FileExtractResult
+    {
+        public FileExtractResult(List<string> models, JsonValueKind kind)
+        {
+            Models = models;
+            ContentKind = kind;
+        }
+
+        readonly public List<string> Models;
+
+        // Used to validate cases such as a single model in an array.
+        readonly public JsonValueKind ContentKind;
+    }
+}
---
+++ clients/dotnet/Azure.Iot.ModelsRepository/ResolverException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Azure.Iot.ModelsRepository
+{
+    public class ResolverException : Exception
+    {
+        public ResolverException(string dtmi) : 
+            base(string.Format(StandardStrings.GenericResolverError, dtmi))
+        {
+        }
+
+        public ResolverException(string dtmi, string message) : 
+            base($"{string.Format(StandardStrings.GenericResolverError, dtmi)}{message}")
+        {
+        }
+
+        public ResolverException(string dtmi, Exception innerException) :
+            base(string.Format(StandardStrings.GenericResolverError, dtmi), innerException)
+        {
+        }
+
+        public ResolverException(string dtmi, string message, Exception innerException) : 
+            base($"{string.Format(StandardStrings.GenericResolverError, dtmi)}{message}", innerException)
+        {
+        }
+    }
+}
